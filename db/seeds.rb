fc2439831cd30164b0d34f8525f08649b21259e2
konatsup <konatsukomatsu@gmail.com>
2020-10-27 17:26:23 +0900

    Fix seeds in case of development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-User.create(
-    name: "テスト",
-    account_name: "test",
-    password: "lit-password",
-    password_confirmation: "lit-password",
-    icon_url: "https://res.cloudinary.com/konatsup/image/upload/v1551697723/qqh7y2n5e5vz40aew1jx.jpg"   
-)
+if development?
+    User.create(
+        name: "テスト",
+        account_name: "test",
+        password: "lit-password",
+        password_confirmation: "lit-password",
+        icon_url: "https://res.cloudinary.com/konatsup/image/upload/v1551697723/qqh7y2n5e5vz40aew1jx.jpg"   
+    )
+end
