57f9b7bfa07e612ab1e393608c1081a301246e62
konatsup <konatsukomatsu@gmail.com>
2020-10-27 15:38:55 +0900

    Add example user to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-<h1>ホーム画面</h1>
+User.create(
+    name: "テスト",
+    account_name: "test",
+    password: "lit-password",
+    password_confirmation: "lit-password",
+    icon_url: "https://res.cloudinary.com/konatsup/image/upload/v1551697723/qqh7y2n5e5vz40aew1jx.jpg"   
+)
